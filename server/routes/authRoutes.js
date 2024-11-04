0f371563dd331fb2e03f6c2b60d12af797adea2e
Benjamin Hill <bennathill@benjamins-mbp.mynetworksettings.com>
2024-11-04 14:20:27 -0800

    Hash password on account creation

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const { test, registerUser } = require("../controllers/authControllers");
 
 router.get("/", test);
-router.get("/register", (req, res) => {
-  res.send("Register page");
-});
 router.post("/register", registerUser);
 
 module.exports = router;
